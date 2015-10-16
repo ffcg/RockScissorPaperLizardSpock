60b79d810b22249edf333bc60291d9aaa77fc5f0
Fredrik Göransson <fredrik.goransson@gmail.com>
2015-10-16 16:42:12 +0200

    Added starting project for xamarin ios

---
+++ src/rspls/AppDelegate.cs
@@ -0,0 +1,15 @@
+using Foundation;
+using UIKit;
+
+namespace FFCG.RSPLS.Frontend.iOS
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to
+	// application events from iOS.
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+		public override UIWindow Window {get; set;}
+	}
+}
